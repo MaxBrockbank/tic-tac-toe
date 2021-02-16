c8ba8a3fd2fba4a78f9bfed7a07e5cdf9f0c9a00
Pugsly99 <rylandadams@yahoo.com>
2021-02-16 15:07:53 -0800

    set step number fail
    Co-authored-by: Max Brockbank <maxbrockbank1999@gmail.com>
    co-authored-by: Randel Moore <Randel.C.Moore@gmail.com>

--- src/__tests__/reducers/stepNumber-reducer.test.js
+++ src/__tests__/reducers/stepNumber-reducer.test.js
@@ -3,7 +3,20 @@ import stepNumberReducer from "../../Reducers/step-number-reducer";
 
 describe('stepNumberReducer', () => {
 let action;
+let history = {
+  key: 1,
+  key2: 2
+}
+
   test('Should return default state', () => {
     expect(stepNumberReducer( 0 , { type:null } )).toEqual(0);
   })
+
+  test('Should set step number equal to object length', () => {
+    const action = {
+      type: 'ADD_STEP',
+      step: Object.keys(history).length,
+    }  
+    expect(stepNumberReducer(0 , action)).toEqual(2)
+  });
 });
