562a0575aa0d1c5b38b1bf7fa84fcd590d0976bf
Pugsly99 <rylandadams@yahoo.com>
2021-02-16 10:45:55 -0800

    fills in X
    Co-authored-by: Max Brockbank <maxbrockbank1999@gmail.com>
    co-authored-by: Randel Moore <Randel.C.Moore@gmail.com>

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -6,14 +6,14 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className = 'game-board'>
-        <Board />
+          <Board />
       </div>
-      <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
       </div>
-    </div>
-    )
+    );
   }
 }
 
